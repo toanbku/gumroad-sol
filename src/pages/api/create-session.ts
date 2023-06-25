b5b4a5876535bb10b709ed6e9953dea1e00310fc
Toan Ho <toanbku@gmail.com>
2023-06-25 17:25:42 +0700

    feat: candypay integrate

---
+++ src/pages/api/create-session.ts
@@ -0,0 +1,34 @@
+import { candypay } from "@/helpers";
+import type { NextApiHandler } from "next";
+
+const handler: NextApiHandler = async (req, res) => {
+  try {
+    const response = await candypay.session.create({
+      success_url: `${process.env.STATIC_URL}/success`,
+      cancel_url: `${process.env.STATIC_URL}/cancel`,
+      // additional SPL tokens, SOL and USDC are the supported tokens by default
+      tokens: ["dust", "samo"],
+      items: [
+        {
+          name: "Solana Shades",
+          // price in USD
+          price: 0.1,
+          image: "https://imgur.com/M0l5SDh.png",
+          quantity: 1,
+          // optional product size parameter
+          size: "9",
+        },
+      ],
+      shipping_fees: 0.5,
+    });
+
+    return res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Error creating session",
+    });
+  }
+};
+
+export default handler;
