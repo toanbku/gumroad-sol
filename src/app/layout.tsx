29c4afa52698084a116f9675883cd91fae953b63
Toan Ho <toanbku@gmail.com>
2023-07-19 13:22:41 +0700

    chore: upgrade package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import SolanaProvider from "@/utils/SolanaProvider";
 import QueryProvider from "@/utils/QueryProvider";
@@ -10,6 +9,11 @@ import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "GumStreet",
+  description: "Selling digital asset and crypto payment",
+};
 
 export default function RootLayout({
   children,
